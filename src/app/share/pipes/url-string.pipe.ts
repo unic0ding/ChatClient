7668ec047b4ebe0cc2f69c82b38157af6f7876f1
basti <basti@boeraker.com>
2017-05-09 02:20:26 +0200

    add ngclass attr for different chatWindow sizes

--- src/app/share/pipes/url-string.pipe.ts
+++ src/app/share/pipes/url-string.pipe.ts
@@ -6,7 +6,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class UrlStringPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    const expr = /(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+    const expr = /(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&/=]*)/;
     if (expr.exec(value)) {
       return value.replace(expr, function (a) {
         return '<a target="_blank" class="msg-text-link" href="' + a + '">' + a + '</a>';
