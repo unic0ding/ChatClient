26b7d13418b9f9d0598e556cd20cf364cf7610bf
basti <basti@boeraker.com>
2017-05-14 03:44:29 +0200

    change socket eventemitter to observable

--- src/app/chat/chat-card/chat-card.component.ts
+++ src/app/chat/chat-card/chat-card.component.ts
@@ -1,9 +1,9 @@
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {Message} from '../../share/model/message.model';
 import {Contact} from '../../share/model/contact.model';
-import {ChatService} from '../../share/services/chat.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Channel} from '../../share/model/channel.model';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-chat-card',
@@ -14,92 +14,77 @@ export class ChatCardComponent implements OnInit, OnDestroy {
   @Output() closeWindow = new EventEmitter();
   @Input() channel: Channel;
   @Input() contact: Contact;
-  messages = [];
-  // chatSubject;
-  socket: WebSocket;
-  listener: EventEmitter<any>;
-  chatForm: FormGroup;
-  notification;
+  private messages = [];
+  private unsentMessages = [];
+  private socket: WebSocket;
+  private chatForm: FormGroup;
+  private notification;
+  private showSpinner = true;
+  private connectionClosed = false;
   private url = 'ws://echo.websocket.org';
-  // private url = 'ws://localhost:8080/echo';
 
 
-  constructor(private chatService: ChatService, private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
     this.contact = new Contact(1, 'Drachenlord', 'altschauerberg8@emskirchen.de');
+
     this.chatForm = this.formBuilder.group({
       message: this.formBuilder.control(null, Validators.required)
     });
-
-    // this.chatSubject = this.chatService.getMessages(this.url);
-    // this.chatSubject.subscribe(
-    //   (msg) => {
-    //     const message = new Message(1, new Date(), new Contact(1, '', ''), msg);
-    //     this.messages.push({message: message, incoming: true});
-    //   },
-    //   (error) => {
-    //     const message = new Message(1, new Date(), new Contact(1, '', ''), 'An error occurred');
-    //     this.messages.push({message: message, incoming: true});
-    //   },
-
-    // (complete) => {
-    //   const message = new Message(1, new Date(), new Contact(1, '', '', '', ''), 'Channel closed');
-    //   this.messages.push({message: message, incoming: true});
-    // }
-    // );
-  }
-
-
-  // sendMessage() {
-  //   let message = this.chatForm.value.message;
-  //   this.chatSubject.next(message);
-  //   message = new Message(1, new Date(), this.contact, message);
-  //   this.messages.push({message: message, incoming: false});
-  //   this.chatForm.reset();
-  // }
-
-  sendMessage() {
-    const message = this.chatForm.value.message;
-    const m = new Message(1, new Date(), this.contact, message);
-    try {
-      this.socket.send(m.toJson());
-    } catch (e) {
-      console.log('Still connecting...');
-    }
-    this.messages.push({message: m, incoming: false});
-    this.chatForm.reset();
   }
 
   ngOnInit() {
     this.socket = new WebSocket(this.url);
-    this.listener = new EventEmitter();
-    this.socket.onopen = event => {
-      this.listener.emit({'type': 'open', 'data': event});
-    };
-    this.socket.onclose = event => {
-      this.listener.emit({'type': 'close', 'data': event});
-    };
-    this.socket.onmessage = event => {
-      this.listener.emit({'type': 'message', 'data': event.data});
-    };
+    const listener = Observable.fromEvent(this.socket, 'message')
+      .map((event) => <MessageEvent>event);
 
-    this.listener.subscribe(event => {
-      if (event.type === 'message') {
+    listener.subscribe((event) => {
         this.updateNotification();
         const message = Message.fromJson(event.data);
         this.messages.push({message: message, incoming: true});
+      },
+      (error) => {
+        console.log(error);
+      },
+
+      () => {
+        const message = new Message(0, new Date(), new Contact(0, 'Socket', ''), 'Socket Connection Closed');
+        this.messages.push({message: message, incoming: true});
+        this.connectionClosed = true;
       }
-      if (event.type === 'close') {
-        const m = new Message(1, new Date(), this.contact, '/The socket connection has been closed');
-        this.messages.push({message: m, incoming: true});
+    );
+
+    const openListener = Observable.fromEvent(this.socket, 'open');
+
+    openListener.subscribe(() => {
+      this.showSpinner = false;
+      for (const message of this.unsentMessages) {
+        this.sendMessage(message);
       }
-      if (event.type === 'open') {
-        const m = new Message(1, new Date(), new Contact(12, 'Socket', ''), '/The socket connection has been established');
-        this.messages.push({message: m, incoming: true});
-      }
+      this.unsentMessages = [];
     });
+
+    const closeListener = Observable.fromEvent(this.socket, 'close')
+      .map((event) => <MessageEvent>event);
+
+    closeListener.subscribe((event) => console.log(event));
   }
 
-  updateNotification(event?) {
+  private sendMessage(message?: Message) {
+    if (!message) {
+      const text = this.chatForm.value.message;
+      message = new Message(1, new Date(), this.contact, text);
+      this.messages.push({message: message, incoming: false});
+      this.chatForm.reset();
+    }
+    try {
+      this.socket.send(message.toJson());
+    } catch (e) {
+      this.unsentMessages.push(message);
+      console.log('Still connecting...');
+    }
+  }
+
+  private updateNotification(event?) {
     if (event === 0) {
       this.notification = null;
       return;
@@ -111,8 +96,11 @@ export class ChatCardComponent implements OnInit, OnDestroy {
     }
   }
 
+  clearHistory() {
+    this.messages = [];
+  }
+
   ngOnDestroy(): void {
     this.socket.close();
-    // this.chatSubject.complete();
   }
 }
